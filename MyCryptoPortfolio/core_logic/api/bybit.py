b7b9159aa29cad27fbf36cab5359ec719cb85acd
sv3t3 <156694882+sv3t3@users.noreply.github.com>
2024-02-13 21:22:24 +0100

    Update bybit.py

--- MyCryptoPortfolio/core_logic/api/bybit.py
+++ MyCryptoPortfolio/core_logic/api/bybit.py
@@ -14,6 +14,24 @@ def fetch_bybit_ticker(symbol):
     response = requests.get(url)
     data = response.json()
     return data['result'][0]['last_price'] if data['result'] else None
+
+from datetime import datetime
+
+def get_bybit_historical_data(coin, interval='D', from_timestamp=None, limit=200):
+    url = 'https://api.bybit.com/public/linear/kline'
+    params = {
+        'symbol': coin + 'USDT',
+        'interval': interval,
+        'from': from_timestamp or int(datetime.now().timestamp()) - 7 * 24 * 60 * 60,  # 7 days ago
+        'limit': limit,
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    if data['ret_code'] == 0:
+        return data['result']
+    else:
+        print(f"Error for coin {coin}: {data['ret_msg']}")
+        return None
     
 # Testing protocol
 # print(fetch_bybit_ticker('ETHUSDT'))
